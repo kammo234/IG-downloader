5cdc173f52850d0b958da4a1dca5ec59c19e2be4
kammo234 <mr.kammo234@gmail.com>
2026-01-02 23:58:27 +0530

    Add Adsterra ad below instruction text in download form

--- src/features/instagram/components/form.tsx
+++ src/features/instagram/components/form.tsx
@@ -7,6 +7,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Download, Loader2 } from "lucide-react";
+import AdsterraNative from "@/components/AdsterraNative";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -103,6 +104,7 @@ export function InstagramVideoForm() {
           Save as video.
         </p>
       </form>
+      <AdsterraNative />
     </Form>
   );
 }
